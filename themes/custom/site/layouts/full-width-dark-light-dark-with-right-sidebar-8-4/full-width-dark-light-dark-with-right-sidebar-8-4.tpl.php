36d57da91a5c36bb8d8c4586afd0f615d5fbc92f
Morten Nissen <mail@hjemmesidekongen.dk>
2018-04-05 14:15:51 +0200

    FAXE-155

--- themes/custom/site/layouts/full-width-dark-light-dark-with-right-sidebar-8-4/full-width-dark-light-dark-with-right-sidebar-8-4.tpl.php
+++ themes/custom/site/layouts/full-width-dark-light-dark-with-right-sidebar-8-4/full-width-dark-light-dark-with-right-sidebar-8-4.tpl.php
@@ -1,14 +1,13 @@
-<!-- full-width-dark-light-dark-with-right-sidebar-8-4.tpl.php -->
 <div <?php if (!empty($css_id)) {
-    echo "id=\"$css_id\"";
+  echo "id=\"$css_id\"";
 } ?>>
 
   <?php if ($content['light_section_0']): ?>
     <!-- Begin - light section no. 0 -->
-    <div class="os2-section os2-section-light">
-      <div class="container">
-        <div class="row">
-          <div class="col-xs-12">
+    <div class="sectioned sectioned--light">
+      <div class="sectioned__inner">
+        <div class="container">
+          <div class="panels-pane-region panels-pane-region--light-section-0">
             <?php echo $content['light_section_0']; ?>
           </div>
         </div>
@@ -18,48 +17,50 @@
   <?php endif; ?>
 
   <?php if ($content['content'] or $content['sidebar']): ?>
-    <!-- Begin - dark section no. 1 -->
-    <div class="os2-section os2-section-dark os2-watermark os2-watermark-light content-primary">
-      <div class="container content-primary">
-        <div class="row">
+    <!-- Begin - dark section -->
+    <div class="sectioned sectioned--dark">
+      <div class="sectioned__inner">
+        <div class="container">
+          <div class="row">
 
-          <?php if ($content['sidebar']): ?>
+            <?php if ($content['sidebar']): ?>
 
-            <!-- Begin - content -->
-            <div class="col-sm-8 content-content">
-              <?php echo $content['content']; ?>
-            </div>
-            <!-- End - content -->
-
-            <!-- Begin - sidebar -->
-            <div class="col-sm-4 hidden-print content-sidebar">
-                <?php echo $content['sidebar']; ?>
-            </div>
-            <!-- End - sidebar -->
-
-          <?php else: ?>
-
-            <!-- Begin - content -->
-            <div class="col-xs-12 content-content">
+              <!-- Begin - content -->
+              <div class="col-sm-8">
                 <?php echo $content['content']; ?>
-            </div>
-            <!-- End - content -->
+              </div>
+              <!-- End - content -->
 
-          <?php endif ?>
+              <!-- Begin - sidebar -->
+              <div class="col-sm-4 hidden-print">
+                <?php echo $content['sidebar']; ?>
+              </div>
+              <!-- End - sidebar -->
 
+            <?php else: ?>
+
+              <!-- Begin - content -->
+              <div class="col-xs-12">
+                <?php echo $content['content']; ?>
+              </div>
+              <!-- End - content -->
+
+            <?php endif ?>
+
+          </div>
         </div>
       </div>
     </div>
-    <!-- End - dark section no. 1 -->
+    <!-- End - dark section -->
   <?php endif ?>
 
   <?php if ($content['light_section_1']): ?>
     <!-- Begin - light section no. 1 -->
-    <div class="os2-section os2-section-light">
-      <div class="container">
-        <div class="row">
-          <div class="col-xs-12 content-light-section-1">
-              <?php echo $content['light_section_1']; ?>
+    <div class="sectioned sectioned--light">
+      <div class="sectioned__inner">
+        <div class="container">
+          <div class="panels-pane-region panels-pane-region--light-section-1">
+            <?php echo $content['light_section_1']; ?>
           </div>
         </div>
       </div>
@@ -69,11 +70,11 @@
 
   <?php if ($content['dark_section_1']): ?>
     <!-- Begin - dark section no. 1 -->
-    <div class="os2-section os2-section-dark">
-      <div class="container">
-        <div class="row">
-          <div class="col-xs-12 content-dark-section-1">
-              <?php echo $content['dark_section_1']; ?>
+    <div class="sectioned sectioned--dark">
+      <div class="sectioned__inner">
+        <div class="container">
+          <div class="panels-pane-region panels-pane-region--dark-section-1">
+            <?php echo $content['dark_section_1']; ?>
           </div>
         </div>
       </div>
--- themes/custom/site/layouts/full-width-dark-light-dark-with-right-sidebar-9-3/full-width-dark-light-dark-with-right-sidebar-9-3.tpl.php
+++ themes/custom/site/layouts/full-width-dark-light-dark-with-right-sidebar-9-3/full-width-dark-light-dark-with-right-sidebar-9-3.tpl.php
@@ -1,51 +1,52 @@
-<!-- full-width-dark-light-dark-with-right-sidebar-9-3.tpl.php -->
 <div <?php if (!empty($css_id)) {
     echo "id=\"$css_id\"";
 } ?>>
 
   <?php if ($content['content'] or $content['sidebar']): ?>
-    <!-- Begin - dark section no. 1 -->
-    <div class="os2-section os2-section-dark os2-watermark os2-watermark-light">
-      <div class="container">
-        <div class="row">
+    <!-- Begin - light section -->
+    <div class="sectioned sectioned--light">
+      <div class="sectioned__inner">
+        <div class="container">
+          <div class="row">
 
-          <?php if ($content['sidebar']): ?>
+            <?php if ($content['sidebar']): ?>
 
-            <!-- Begin - content -->
-            <div class="col-sm-9">
-              <?php echo $content['content']; ?>
-            </div>
-            <!-- End - content -->
-
-            <!-- Begin - sidebar -->
-            <div class="col-sm-3 hidden-print">
-                <?php echo $content['sidebar']; ?>
-            </div>
-            <!-- End - sidebar -->
-
-          <?php else: ?>
-
-            <!-- Begin - content -->
-            <div class="col-xs-12">
+              <!-- Begin - content -->
+              <div class="col-sm-9">
                 <?php echo $content['content']; ?>
-            </div>
-            <!-- End - content -->
+              </div>
+              <!-- End - content -->
 
-          <?php endif ?>
+              <!-- Begin - sidebar -->
+              <div class="col-sm-3 hidden-print">
+                  <?php echo $content['sidebar']; ?>
+              </div>
+              <!-- End - sidebar -->
 
+            <?php else: ?>
+
+              <!-- Begin - content -->
+              <div class="col-xs-12">
+                  <?php echo $content['content']; ?>
+              </div>
+              <!-- End - content -->
+
+            <?php endif ?>
+
+          </div>
         </div>
       </div>
     </div>
-    <!-- End - dark section no. 1 -->
+    <!-- End - light section -->
   <?php endif ?>
 
   <?php if ($content['light_section_1']): ?>
     <!-- Begin - light section no. 1 -->
-    <div class="os2-section os2-section-light">
-      <div class="container">
-        <div class="row">
-          <div class="col-xs-12">
-              <?php echo $content['light_section_1']; ?>
+    <div class="sectioned sectioned--light">
+      <div class="sectioned__inner">
+        <div class="container">
+          <div class="panels-pane-region panels-pane-region--light-section-1">
+            <?php echo $content['light_section_1']; ?>
           </div>
         </div>
       </div>
@@ -53,18 +54,18 @@
     <!-- End - light section no. 1 -->
   <?php endif; ?>
 
-  <?php if (!empty($content['dark_section_2'])): ?>
-    <!-- Begin - dark section no. 2 -->
-    <div class="os2-section os2-section-dark">
-      <div class="container">
-        <div class="row">
-          <div class="col-xs-12">
-              <?php echo $content['dark_section_2']; ?>
+  <?php if ($content['dark_section_1']): ?>
+    <!-- Begin - dark section no. 1 -->
+    <div class="sectioned sectioned--dark">
+      <div class="sectioned__inner">
+        <div class="container">
+          <div class="panels-pane-region panels-pane-region--dark-section-1">
+            <?php echo $content['dark_section_1']; ?>
           </div>
         </div>
       </div>
     </div>
-    <!-- End - dark section no. 2 -->
+    <!-- End - dark section no. 1 -->
   <?php endif; ?>
 
 </div>
