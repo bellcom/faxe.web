cc9c0176902dc9e05d7a8531407a501c47ca64de
Morten Nissen <mail@hjemmesidekongen.dk>
2018-04-11 14:14:06 +0200

    FAXE-155

--- themes/custom/site/layouts/full-width-light-dark-light/full-width-light-dark-light.tpl.php
+++ themes/custom/site/layouts/full-width-light-dark-light/full-width-light-dark-light.tpl.php
@@ -5,49 +5,74 @@
 
   <?php if ($content['light_section_1']): ?>
     <!-- Begin - light section no. 1 -->
-    <div class="os2-section os2-section-light os2-watermark os2-watermark-dark">
-      <div class="container">
-        <div class="row">
-
-          <div class="col-xs-12">
-              <?php echo $content['light_section_1']; ?>
+    <div class="sectioned sectioned--light">
+      <div class="sectioned__inner">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12">
+              <div class="panels-pane-region panels-pane-region--light-section-1">
+                <?php echo $content['light_section_1']; ?>
+              </div>
+            </div>
           </div>
-
+        </div>
       </div>
     </div>
-  </div>
-  <!-- End - light section no. 1 -->
+    <!-- End - light section no. 1 -->
   <?php endif; ?>
 
   <?php if ($content['dark_section_1']): ?>
     <!-- Begin - dark section no. 1 -->
-    <div class="os2-section os2-section-dark">
-      <div class="container">
-        <div class="row">
-
-          <div class="col-xs-12">
-              <?php echo $content['dark_section_1']; ?>
+    <div class="sectioned sectioned--dark">
+      <div class="sectioned__inner">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12">
+              <div class="panels-pane-region panels-pane-region--dark-section-1">
+                <?php echo $content['dark_section_1']; ?>
+              </div>
+            </div>
           </div>
+        </div>
       </div>
     </div>
-  </div>
-  <!-- End - dark section no. 1 -->
+    <!-- End - dark section no. 1 -->
   <?php endif; ?>
 
   <?php if ($content['light_section_2']): ?>
     <!-- Begin - light section no. 2 -->
-    <div class="os2-section os2-section-light">
-      <div class="container">
-        <div class="row">
-
-          <div class="col-xs-12">
-              <?php echo $content['light_section_2']; ?>
+    <div class="sectioned sectioned--light">
+      <div class="sectioned__inner">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12">
+              <div class="panels-pane-region panels-pane-region--light-section-2">
+                <?php echo $content['light_section_2']; ?>
+              </div>
+            </div>
           </div>
-
+        </div>
       </div>
     </div>
-  </div>
-  <!-- End - light section no. 2 -->
+    <!-- End - light section no. 2 -->
+  <?php endif; ?>
+
+  <?php if ($content['dark_section_2']): ?>
+    <!-- Begin - dark section no. 2 -->
+    <div class="sectioned sectioned--dark">
+      <div class="sectioned__inner">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12">
+              <div class="panels-pane-region panels-pane-region--dark-section-2">
+                <?php echo $content['dark_section_2']; ?>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End - dark section no. 2 -->
   <?php endif; ?>
 
 </div>
