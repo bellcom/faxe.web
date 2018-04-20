7447be486c4955a4b091462ecc34f2e0631f3be1
Morten Nissen <mail@hjemmesidekongen.dk>
2018-04-20 15:04:12 +0200

    FAXE-171

--- themes/custom/site/templates/node/full/node--bc-hero--full.tpl.php
+++ themes/custom/site/templates/node/full/node--bc-hero--full.tpl.php
@@ -7,32 +7,41 @@ if (isset($node->field_bc_hero_image['und']['0']['uri']) && $uri = $node->field_
 }
 ?>
 
-<article style="background-image: url('<?php print $image_url; ?>');"
-         id="node-<?php print $node->nid; ?>"
+<article id="node-<?php print $node->nid; ?>"
          class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-  <div class="row row--equal-height-columns">
-    <div class="col-xs-12 col-sm-7 col-md-8">
-
-      <?php if (isset($content['field_bc_hero_slides'])): ?>
-        <!-- Begin - slides -->
-        <div class="node__slides">
-          <?php print render($content['field_bc_hero_slides']); ?>
-        </div>
-        <!-- End - slides -->
-      <?php endif; ?>
-
+  <?php if (!empty($title)): ?>
+    <!-- Begin - pre-heading -->
+    <div class="node__preheading">
+      <h3 class="node__preheading__title"><?php print $title; ?></h3>
     </div>
-    <div class="col-xs-12 col-sm-5 col-md-4">
+    <!-- End - pre-heading -->
+  <?php endif; ?>
 
-      <?php if (isset($content['field_bc_hero_links'])): ?>
-        <!-- Begin - links -->
-        <div class="node__links">
-          <?php print render($content['field_bc_hero_links']); ?>
-        </div>
-        <!-- End - links -->
-      <?php endif; ?>
+  <div class="node__inner" style="background-image: url('<?php print $image_url; ?>');">
+    <div class="row row--equal-height-columns">
+      <div class="col-xs-12 col-sm-7 col-md-8">
 
+        <?php if (isset($content['field_bc_hero_slides'])): ?>
+          <!-- Begin - slides -->
+          <div class="node__slides">
+            <?php print render($content['field_bc_hero_slides']); ?>
+          </div>
+          <!-- End - slides -->
+        <?php endif; ?>
+
+      </div>
+      <div class="col-xs-12 col-sm-5 col-md-4">
+
+        <?php if (isset($content['field_bc_hero_links'])): ?>
+          <!-- Begin - links -->
+          <div class="node__links">
+            <?php print render($content['field_bc_hero_links']); ?>
+          </div>
+          <!-- End - links -->
+        <?php endif; ?>
+
+      </div>
     </div>
   </div>
 
