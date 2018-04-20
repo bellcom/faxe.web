4b46e0aa8c9804ad5e7f6a449f100fb872bacb6b
Morten Nissen <mail@hjemmesidekongen.dk>
2018-04-20 12:33:26 +0200

    FAXE-164

--- themes/custom/site/templates/search/search-api-page-result.tpl.php
+++ themes/custom/site/templates/search/search-api-page-result.tpl.php
@@ -3,36 +3,36 @@ $title = (strlen($title) > 45) ? substr($title, 0, 45) . '...' : $title;
 ?>
 
 <div class="search-results__list__item">
-  <a href="<?php print url($url['path']); ?>" class="element-wrapper-link">
-    <div class="entity-list-advanced entity-list-advanced--search-result">
+    <a href="<?php print url($url['path']); ?>" class="element-wrapper-link">
+        <div class="entity-list-advanced entity-list-advanced--search-result">
 
-      <div class="entity-list-advanced__body">
+            <div class="entity-list-advanced__body">
 
-        <div class="entity-list-advanced__heading">
-          <h3 class="entity-list-advanced__heading__title heading-h4"><?php print check_plain($title); ?></h3>
-        </div>
+                <div class="entity-list-advanced__heading">
+                    <h3 class="entity-list-advanced__heading__title heading-h4"><?php print check_plain($title); ?></h3>
+                </div>
 
-        <span class="entity-list-advanced__path">
+                <span class="entity-list-advanced__path">
           <?php print t('Located at: !path', array('!path' => drupal_get_path_alias($url['path']))); ?>
         </span>
 
-        <?php if ($snippet OR $info) : ?>
-          <div class="entity-list-advanced__search-snippet">
+              <?php if ($snippet OR $info) : ?>
+                  <div class="entity-list-advanced__search-snippet">
 
-            <?php if ($snippet) : ?>
-              <p><?php print $snippet; ?></p>
-            <?php endif; ?>
+                    <?php if ($snippet) : ?>
+                        <p><?php print strip_tags($snippet, '<br>'); ?></p>
+                    <?php endif; ?>
 
-            <?php if ($info) : ?>
-              <p><?php print $info; ?></p>
-            <?php endif; ?>
+                    <?php if ($info) : ?>
+                        <p><?php print strip_tags($info, '<br>'); ?></p>
+                    <?php endif; ?>
 
-          </div>
-        <?php endif; ?>
+                  </div>
+              <?php endif; ?>
 
-      </div>
+            </div>
 
-    </div>
+        </div>
 
-  </a>
+    </a>
 </div>
