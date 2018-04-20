782aae980284bd73ea5b128945984e3d0d0144e4
Morten Nissen <mail@hjemmesidekongen.dk>
2018-04-20 12:33:43 +0200

    FAXE-149

---
+++ themes/custom/site/templates/node/full/node--os2web-base-iframe--full.tpl.php
@@ -0,0 +1,28 @@
+<article id="node-<?php print $node->nid; ?>"
+         class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <?php if (!empty($title)): ?>
+    <!-- Begin - heading -->
+    <div class="node__heading">
+      <h1 class="node__heading__title"><?php print $title; ?></h1>
+    </div>
+    <!-- End - heading -->
+  <?php endif; ?>
+
+  <?php if (isset($content['field_os2web_base_field_text'])): ?>
+    <!-- Begin - intro -->
+    <div class="node__intro">
+      <?php print render($content['field_os2web_base_field_text']); ?>
+    </div>
+    <!-- End - intro -->
+  <?php endif; ?>
+
+  <?php if (isset($content['body'])): ?>
+    <!-- Begin - body -->
+    <div class="node__body">
+      <?php print render($content['body']); ?>
+    </div>
+    <!-- End - body -->
+  <?php endif; ?>
+
+</article>
