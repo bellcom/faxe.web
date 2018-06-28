b68051bd91223192318fecc2b621d25b92754be2
Morten Nissen <mail@hjemmesidekongen.dk>
2018-06-28 15:11:30 +0200

    FAXE-197

--- themes/custom/site/templates/block/block--sidebar--left.tpl.php
+++ themes/custom/site/templates/block/block--sidebar--left.tpl.php
@@ -1,14 +1,14 @@
-<div id="<?php print $block_html_id; ?>" class="boxy boxy--facets <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<div id="<?php print $block_html_id; ?>" class="partial partial--facets <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <?php print render($title_prefix); ?>
   <?php if ($title): ?>
-  <div class="boxy__heading">
-    <h2 class="boxy__heading__title"<?php print $title_attributes; ?>><?php print $title; ?></h2>
+  <div class="partial__heading">
+    <h2 class="partial__heading__title"<?php print $title_attributes; ?>><?php print $title; ?></h2>
   </div>
   <?php endif;?>
   <?php print render($title_suffix); ?>
 
-  <div class="boxy__body">
+  <div class="partial__body">
     <?php print $content ?>
   </div>
 
