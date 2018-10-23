8b0fc99f3d6f3ab650512cf294f6dfce71191c28
Morten Nissen <mail@hjemmesidekongen.dk>
2018-10-23 16:35:19 +0200

    Responsive navigatoin

--- themes/custom/bellcom/template.php
+++ themes/custom/bellcom/template.php
@@ -39,6 +39,9 @@ function bellcom_preprocess_page(&$variables) {
   $variables['flexy_navigation__primary'] = _bellcom_generate_menu($primary_navigation_name, 'flexy_navigation', TRUE);
   $variables['flexy_navigation__secondary'] = _bellcom_generate_menu($secondary_navigation_name, 'flexy_navigation', TRUE);
 
+  $variables['menu_slinky_custom__primary'] = _bellcom_generate_menu($primary_navigation_name, 'slinky-custom', TRUE);
+  $variables['menu_slinky_custom__secondary'] = _bellcom_generate_menu($secondary_navigation_name, 'slinky-custom', TRUE);
+
   $variables['menu_slinky__primary'] = _bellcom_generate_menu($primary_navigation_name, 'slinky', TRUE);
   $variables['menu_slinky__secondary'] = _bellcom_generate_menu($secondary_navigation_name, 'slinky', TRUE);
 
@@ -184,6 +187,14 @@ function bellcom_menu_tree__slinky(&$variables) {
 
 /*
  * Implements theme_menu_tree().
+ * For custom slinky menu types.
+ */
+function bellcom_menu_tree__slinky_custom(&$variables) {
+  return $variables['tree'];
+}
+
+/*
+ * Implements theme_menu_tree().
  * For slinky menu types.
  */
 function bellcom_menu_tree__flexy_list(&$variables) {
@@ -298,6 +309,36 @@ function bellcom_menu_link__slinky(array $variables) {
 
     $element['#localized_options']['fragment'] = 'content';
     $element['#localized_options']['external'] = TRUE;
+  }
+
+  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+
+  return '<li>' . $output . $sub_menu . "</li>\n";
+}
+
+/*
+ * Implements theme_menu_link().
+ */
+function bellcom_menu_link__slinky_custom(array $variables) {
+  $element = $variables['element'];
+  $sub_menu = '';
+
+  if ($element['#below']) {
+
+    // Prevent dropdown functions from being added to management menu so it
+    // does not affect the navbar module.
+    if (($element['#original_link']['menu_name'] == 'management') && (module_exists('navbar'))) {
+      $sub_menu = drupal_render($element['#below']);
+    }
+
+    elseif ((!empty($element['#original_link']['depth']))) {
+
+      // Add our own wrapper.
+      unset($element['#below']['#theme_wrappers']);
+
+      // Submenu classes
+      $sub_menu = ' <ul>' . drupal_render($element['#below']) . '</ul>';
+    }
   }
 
   $output = l($element['#title'], $element['#href'], $element['#localized_options']);
