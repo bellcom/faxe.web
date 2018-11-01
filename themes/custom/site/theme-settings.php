8d8602bfc19d8dfe54619d77462b98609da38b41
Morten Nissen <mail@hjemmesidekongen.dk>
2018-11-01 11:10:36 +0100

    Theme settings

--- themes/custom/site/theme-settings.php
+++ themes/custom/site/theme-settings.php
@@ -1,5 +1,25 @@
 <?php
 
+// Custom submit function.
+function _theme_site_submit(&$form, $form_state, $form_id = NULL) {
+  $current_theme = variable_get('theme_default', 'none');
+
+  $fid = $form_state['values']['header_background_image'];
+  $file = file_load($fid);
+
+  if (is_object($file)) {
+
+    // Check to make sure that the file is set to be permanent.
+    if ($file->status == 0) {
+
+      // Update the status.
+      $file->status = FILE_STATUS_PERMANENT;
+      file_save($file);
+      file_usage_add($file, $current_theme, 'theme', 1);
+    }
+  }
+}
+
 /**
  * Implements hook_form_FORM_ID_alter().
  */
@@ -22,8 +42,8 @@ function site_form_system_theme_settings_alter(&$form, $form_state, $form_id = N
 
   // Background image
   $form['project']['header']['header_background_image'] = array(
-    '#type'     => 'managed_file',
-    '#title'    => t('Background image'),
+    '#type' => 'managed_file',
+    '#title' => t('Background image'),
     '#description' => t('Should be width: 1675px, height: 300px'),
     '#required' => TRUE,
     '#upload_location' => file_default_scheme() . '://theme/',
@@ -33,24 +53,5 @@ function site_form_system_theme_settings_alter(&$form, $form_state, $form_id = N
     ),
   );
 
-  $form['#submit'][] = 'site_form_system_theme_settings_submit';
-}
-
-function site_form_system_theme_settings_submit(&$form, $form_state, $form_id = NULL) {
-  $current_theme = variable_get('theme_default', 'none');
-
-  $fid = $form_state['values']['header_background_image'];
-  $file = file_load($fid);
-
-  if (is_object($file)) {
-
-    // Check to make sure that the file is set to be permanent.
-    if ($file->status == 0) {
-
-      // Update the status.
-      $file->status = FILE_STATUS_PERMANENT;
-      file_save($file);
-      file_usage_add($file, $current_theme, 'theme', 1);
-    }
-  }
+  $form['#submit'][] = '_theme_site_submit';
 }
