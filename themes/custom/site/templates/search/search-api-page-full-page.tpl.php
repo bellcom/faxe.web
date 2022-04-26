0e4f8696d3acfda56aa53364ddc7620fa9efa49c
Stanislav Kutasevits <stan@bellcom.dk>
2022-04-26 14:41:38 +0300

    FAXE-267 links location, meetings search optimization

--- themes/custom/site/templates/search/search-api-page-full-page.tpl.php
+++ themes/custom/site/templates/search/search-api-page-full-page.tpl.php
@@ -34,10 +34,38 @@
   </div>
   <!-- End - form -->
 
+  <!-- Begin - filtering -->
+  <h2>
+    Præciser søgningen efter netop dit behov
+  </h2>
+  <p>
+    Klik og tilpas søgnigen præcis efter dit formål med søgningen. Ønsker du at gå tilbage til den
+    fulde søgning, så kan du blot trykke på dem igen, og indholdet går tilbage til det originale.
+  </p>
+
+  <?php
+  $q = $_GET['q'];
+  $qParts = explode('/', $q);
+  $searchPage = $qParts[0];
+  $searchTerm = $qParts[1];
+  ?>
+  <a href="/sog/<?php print $searchTerm?>" class="<?php if ($searchPage == 'sog') print 'active' ?>">
+    Fuld søgning
+  </a>
+  <a href="/sog-dagsorden/<?php print $searchTerm?>" class="<?php if ($searchPage == 'sog-dagsorden') print 'active' ?>">
+    Dagsordener og referater
+  </a>
+  <a href="/sog-selvbetjening/<?php print $searchTerm?>" class="<?php if ($searchPage == 'sog-selvbetjening') print 'active' ?>">
+    Selvbetjening
+  </a>
+  <!-- End - filtering -->
+
   <!-- Begin - list -->
   <div class="search-results__list">
     <?php print render($results); ?>
   </div>
   <!-- End - list -->
+
+
 
 </div>
